545bb3edc7afff8fed740c5508f04dd7c09bf1de
Garrett Dodd <garrettdodd03@gmail.com>
2022-09-01 15:10:51 -0500

    changes during class

---
+++ BRAVO_LAB/scripts/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    console.log($('#doge').attr("src"));
+    let i = 0;
+    const pictures = ["assets/standingDoge.png", "assets/delicious.png", 'https://www.minecraftskins.net/static/preview/doge.png', 'assets/funny.jpg']
+    $('#doge').click(function(){
+        if(i === 0){
+            $('#doge').attr("src", pictures[1])
+            i++
+        }
+        else if (i === 1){
+            $('#doge').attr("src", pictures[2])
+            i++
+        }
+        else if (i ===2){
+            $('#doge').attr("src", pictures[3])
+            i++
+        }
+        else{
+            $('#doge').attr("src", pictures[0])
+            i=0
+        }
+        // $('#doge').attr("src", pictures[1])
+    })
+});
