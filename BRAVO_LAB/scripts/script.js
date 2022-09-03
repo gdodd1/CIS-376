4564e78e954118f42deb95ef05cb9cfaf3cee6bc
gdodd1 <garrettdodd03@gmail.com>
2022-09-03 00:04:47 -0500

    optimize jQuery algorithm for switching pitcures

--- BRAVO_LAB/scripts/script.js
+++ BRAVO_LAB/scripts/script.js
@@ -3,21 +3,12 @@ $(document).ready(function(){
     let i = 0;
     const pictures = ["assets/standingDoge.png", "assets/delicious.png", 'https://www.minecraftskins.net/static/preview/doge.png', 'assets/funny.jpg']
     $('#doge').click(function(){
-        if(i === 0){
-            $('#doge').attr("src", pictures[1])
-            i++
-        }
-        else if (i === 1){
-            $('#doge').attr("src", pictures[2])
-            i++
-        }
-        else if (i ===2){
-            $('#doge').attr("src", pictures[3])
-            i++
+        if(i < 3){
+            $('#doge').attr("src", pictures[++i])
         }
         else{
             $('#doge').attr("src", pictures[0])
             i=0
-        }
+                }
     })
 });
